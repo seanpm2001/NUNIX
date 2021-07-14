87cdb460cf46fff98d194f7cf6f4baf650a10772
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-07-13 19:44:48 -0700

    PYTHON_V1.hap.py
    An archive of the first version of the HAP suite file for Python.

---
+++ HAP/Structure/ProgrammingLanguages/Python/OldVersions/1/1-100/PYTHON_V1.hap.py
@@ -0,0 +1,12 @@
+# NUNIX idea: HAP files will have swappable language support via the magic number at the beginning of each file. If someone is a Pythoneer or Pythonista at heart, they can choose Python as their default language for what they want to be written in. Likewise, other languages can be swapped for, such as Java, Kotlin, C, C++, C#, Assembly, TypeScript, JavaScript, SQL, HTML, R, REBOL, Vim Script, Emacs Lisp, LISP, Scheme, Racket, F#, Boo, Vala, Genie, Forth, FORTRAN, ALGOL, Go!, Go, etc. all languages will be supported
+# <code>
+<#BIN/MAGIC/$SET="PythonCompat">
+import pythonCompat
+# [CR]
+# NUNIX
+# [CR]
+# Program file
+# Still uses NUNIX keywords, but with Python styled programming
+# [CR]
+print("Good morning, programmer") N>CLI # Outputs to the NUNIX CLI
+# </code>
